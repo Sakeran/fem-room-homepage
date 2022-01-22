f555cd661caffbadab44e20d343197dc27ad50cb
Alexander <Sakeran@gmail.com>
2022-01-22 15:32:51 -0500

    Implement nav toggle

---
+++ src/components/nav-toggle.js
@@ -0,0 +1,24 @@
+// Mobile menu toggle
+
+(() => {
+  const toggle = document.getElementById("nav-toggle");
+  const nav = document.querySelector("nav");
+
+  if (!(toggle && nav)) return;
+
+  const toggleButton = () => {
+    toggle.setAttribute(
+      "aria-expanded",
+      toggle.getAttribute("aria-expanded") ? "false" : "true"
+    );
+  };
+
+  const toggleNav = () => {
+    nav.classList.toggle("menu-visible");
+  };
+
+  toggle.addEventListener("click", () => {
+    toggleButton();
+    toggleNav();
+  });
+})();
